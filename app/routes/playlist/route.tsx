9cbec5cb71308e9591eaf3fc3adf008a28e2d15b
shubhswain45 <shubhkantswain@.com>
2025-01-29 20:39:23 +0530

    p

--- app/routes/playlist/route.tsx
+++ app/routes/playlist/route.tsx
@@ -1,3 +1,4 @@
+import { MoreHorizontal } from 'lucide-react';
 import React from 'react';
 // import { Play, Pause, SkipForward, Heart, MoreHorizontal, Clock3 } from 'lucide-react';
 import MusicPlayerLayout from '~/components/layout';
@@ -7,7 +8,7 @@ const PlaylistPage = () => {
 
   const playlist = {
     title: "Today's Top Hits",
-    coverImage: 'https://m.media-amazon.com/images/I/51QsSipMLdL._UX358_FMwebp_QL85_.jpg',
+    coverImage: 'https://m.media-amazon.com/images/I/41u8hprs3sL._UX250_FMwebp_QL85_.jpg',
     description: "The most popular tracks right now",
     totalSongs: 50,
     duration: "2 hr 45 min",
@@ -17,35 +18,97 @@ const PlaylistPage = () => {
         artist: "Artist Name",
         album: "Album Name",
         duration: "3:24",
-        image: "/api/placeholder/60/60"
+        image: "https://m.media-amazon.com/images/I/41Z7Z9uYlEL._SX354_SY354_BL0_QL100__UX358_FMwebp_QL85_.jpg",
+        dateAdded: "2 days ago"
       },
       {
         title: "Song Two",
         artist: "Artist Name",
         album: "Album Name",
         duration: "4:12",
-        image: "/api/placeholder/60/60"
+        image: "https://m.media-amazon.com/images/I/51Ls+9riaXL._SX354_SY354_BL0_QL100__UX358_FMwebp_QL85_.jpg",
+        dateAdded: "3 days ago"
       },
       {
         title: "Song Three",
         artist: "Artist Name",
         album: "Album Name",
         duration: "3:18",
-        image: "/api/placeholder/60/60"
+        image: "https://m.media-amazon.com/images/I/51PndTwTN1L._SX354_SY354_BL0_QL100__UX358_FMwebp_QL85_.jpg",
+        dateAdded: "5 days ago"
       },
       {
         title: "Song Four",
         artist: "Artist Name",
         album: "Album Name",
         duration: "3:55",
-        image: "/api/placeholder/60/60"
+        image: "https://m.media-amazon.com/images/I/41-Ia7Xl3vL._SX354_SY354_BL0_QL100__UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
       },
       {
         title: "Song Five",
         artist: "Artist Name",
         album: "Album Name",
         duration: "3:30",
-        image: "/api/placeholder/60/60"
+        image: "https://m.media-amazon.com/images/I/51iq1zWdgwL._SX354_SY354_BL0_QL100__UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
+      },
+
+      {
+        title: "Song Six",
+        artist: "Artist Name",
+        album: "Album Name",
+        duration: "3:30",
+        image: "https://m.media-amazon.com/images/S/dmp-catalog-images-prod/images/4731f943-5683-4832-b200-145fe1dc6172/4731f943-5683-4832-b200-145fe1dc6172--1619766509._UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
+      },
+      {
+        title: "Song Six",
+        artist: "Artist Name",
+        album: "Album Name",
+        duration: "3:30",
+        image: "https://m.media-amazon.com/images/S/dmp-catalog-images-prod/images/4731f943-5683-4832-b200-145fe1dc6172/4731f943-5683-4832-b200-145fe1dc6172--1619766509._UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
+      },
+      {
+        title: "Song Six",
+        artist: "Artist Name",
+        album: "Album Name",
+        duration: "3:30",
+        image: "https://m.media-amazon.com/images/S/dmp-catalog-images-prod/images/4731f943-5683-4832-b200-145fe1dc6172/4731f943-5683-4832-b200-145fe1dc6172--1619766509._UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
+      },
+      {
+        title: "Song Six",
+        artist: "Artist Name",
+        album: "Album Name",
+        duration: "3:30",
+        image: "https://m.media-amazon.com/images/S/dmp-catalog-images-prod/images/4731f943-5683-4832-b200-145fe1dc6172/4731f943-5683-4832-b200-145fe1dc6172--1619766509._UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
+      },
+      {
+        title: "Song Six",
+        artist: "Artist Name",
+        album: "Album Name",
+        duration: "3:30",
+        image: "https://m.media-amazon.com/images/S/dmp-catalog-images-prod/images/4731f943-5683-4832-b200-145fe1dc6172/4731f943-5683-4832-b200-145fe1dc6172--1619766509._UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
+      },
+      {
+        title: "Song Six",
+        artist: "Artist Name",
+        album: "Album Name",
+        duration: "3:30",
+        image: "https://m.media-amazon.com/images/S/dmp-catalog-images-prod/images/4731f943-5683-4832-b200-145fe1dc6172/4731f943-5683-4832-b200-145fe1dc6172--1619766509._UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
+      },
+      {
+        title: "Song Six",
+        artist: "Artist Name",
+        album: "Album Name",
+        duration: "3:30",
+        image: "https://m.media-amazon.com/images/S/dmp-catalog-images-prod/images/4731f943-5683-4832-b200-145fe1dc6172/4731f943-5683-4832-b200-145fe1dc6172--1619766509._UX358_FMwebp_QL85_.jpg",
+        dateAdded: "1 week ago"
       }
     ]
   };
@@ -53,7 +116,6 @@ const PlaylistPage = () => {
   return (
     <MusicPlayerLayout>
       <div className="text-white relative min-h-screen">
-        {/* Background Gradient */}
         <div 
           className="absolute inset-0 bg-gradient-to-b from-transparent to-black z-0"
           style={{
@@ -65,15 +127,13 @@ const PlaylistPage = () => {
           }}
         />
 
-        {/* Content */}
         <div className="relative z-10">
           <div className="p-4 sm:p-6 md:p-8">
-            {/* Header Section */}
             <div className="py-8 md:py-12 flex flex-col md:flex-row items-center md:items-start gap-8">
               <img
                 src={playlist.coverImage}
                 alt={playlist.title}
-                className="w-60 h-60 md:w-64 md:h-64 rounded-lg shadow-xl"
+                className="w-56 h-56 md:w-64 md:h-64 rounded-lg shadow-xl"
               />
               <div className="flex flex-col items-center md:items-start gap-4 text-center md:text-left">
                 <h1 className="text-4xl md:text-5xl font-bold">{playlist.title}</h1>
@@ -81,7 +141,6 @@ const PlaylistPage = () => {
                 <div className="text-sm text-gray-400">
                   {playlist.totalSongs} songs • {playlist.duration}
                 </div>
-                
                 <div className="flex items-center gap-4 mt-4">
                   <button 
                     onClick={() => setIsPlaying(!isPlaying)}
@@ -94,27 +153,28 @@ const PlaylistPage = () => {
               </div>
             </div>
 
-            {/* Tracks Section */}
             <div className="pb-8">
               <table className="w-full">
                 <thead>
                   <tr className="text-gray-400 border-b border-gray-800">
-                    <th className="text-left py-4 w-16">#</th>
+                    <th className="text-left py-4 w-12 pl-4">#</th>
                     <th className="text-left">Title</th>
                     <th className="text-left hidden md:table-cell">Album</th>
-                    <th className="text-right pr-8">
-                      {/* <Clock3 size={16} /> */}
-                    </th>
-                    <th className="w-8"></th>
+                    <th className="text-left hidden md:table-cell">Date Added</th>
+                    <th className="text-right hidden md:table-cell w-24">
+                      {/* <Clock3 size={16} className="ml-auto mr-8" /> */}
+                      <div className="ml-auto mr-8">clock</div>
+                      </th>
+                    <th className="text-right w-16">More</th>
                   </tr>
                 </thead>
                 <tbody>
                   {playlist.tracks.map((track, index) => (
                     <tr 
                       key={index}
-                      className="hover:bg-gray-800/50 group"
+                      className="hover:bg-gray-800/50 group rounded-lg border-b border-gray-700 mt-4"
                     >
-                      <td className="py-4">{index + 1}</td>
+                      <td className="py-6 pl-4">{index + 1}</td>
                       <td>
                         <div className="flex items-center gap-4">
                           <img
@@ -129,12 +189,10 @@ const PlaylistPage = () => {
                         </div>
                       </td>
                       <td className="hidden md:table-cell text-gray-400">{track.album}</td>
-                      <td className="text-right text-gray-400 pr-8">{track.duration}</td>
-                      <td>
-                        <div className="flex items-center justify-end gap-4 opacity-0 group-hover:opacity-100">
-                          {/* <Heart size={16} className="cursor-pointer hover:text-green-500" /> */}
-                          {/* <MoreHorizontal size={16} className="cursor-pointer" /> */}
-                        </div>
+                      <td className="hidden md:table-cell text-gray-400">{track.dateAdded}</td>
+                      <td className="text-right hidden md:table-cell text-gray-400 pr-8">{track.duration}</td>
+                      <td className="text-right pr-4">
+                        {/* <MoreHorizontal size={16} className="cursor-pointer text-gray-400 hover:text-white ml-auto" /> */}
                       </td>
                     </tr>
                   ))}
