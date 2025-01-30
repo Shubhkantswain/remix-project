09466ec188fb20bf0830b8ab49b19e573384c8a0
shubhswain45 <shubhkantswain@.com>
2025-01-30 16:49:14 +0530

    k

--- app/routes/_app.explore.playlists/route.tsx
+++ app/routes/_app.explore.playlists/route.tsx
@@ -203,9 +203,9 @@ function ExplorePlaylistsPage() {
                                                 </div>
                                             </div>
                                         </td>
-                                        <td className={`hidden md:table-cell text-gray-400 ${index == currentIdx ? "text-[#fa586a]" : ""}`}>{track.album}</td>
-                                        <td className={`hidden md:table-cell text-gray-400 ${index == currentIdx ? "text-[#fa586a]" : ""}`}>{track.dateAdded}</td>
-                                        <td className={`text-right hidden md:table-cell text-gray-400 pr-8 ${index == 2 ? "text-[#fa586a]" : ""}`}>{track.duration}</td>
+                                        <td className={`hidden md:table-cell  ${index == currentIdx ? "text-[#fa586a]" : "text-gray-400"}`}>{track.album}</td>
+                                        <td className={`hidden md:table-cell  ${index == currentIdx ? "text-[#fa586a]" : "text-gray-400"}`}>{track.dateAdded}</td>
+                                        <td className={`text-right hidden md:table-cell  pr-8 ${index == 2 ? "text-[#fa586a]" : "text-gray-400"}`}>{track.duration}</td>
                                         <td className="text-right pr-4">
                                             <button className="cursor-pointer text-gray-400 hover:text-white ml-auto">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><defs><path id="ic_action_more-a" d="M19,14 C17.895,14 17,13.105 17,12 C17,10.895 17.895,10 19,10 C20.105,10 21,10.895 21,12 C21,13.105 20.105,14 19,14 Z M14,12 C14,10.895 13.105,10 12,10 C10.895,10 10,10.895 10,12 C10,13.105 10.895,14 12,14 C13.105,14 14,13.105 14,12 Z M7,12 C7,10.895 6.105,10 5,10 C3.895,10 3,10.895 3,12 C3,13.105 3.895,14 5,14 C6.105,14 7,13.105 7,12 Z"></path></defs><g fillRule="evenodd" fill="transparent"><rect width="24" height="24"></rect><use fill-rule="nonzero" href="#ic_action_more-a" fill="currentColor"></use></g></svg>
