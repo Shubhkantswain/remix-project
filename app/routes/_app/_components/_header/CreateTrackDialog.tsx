5ff105427945d99a9c6f784d1d24bf156f9d3320
shubhswain45 <shubhkantswain@.com>
2025-02-05 19:46:07 +0530

    oo

--- app/routes/_app/_components/_header/CreateTrackDialog.tsx
+++ app/routes/_app/_components/_header/CreateTrackDialog.tsx
@@ -1,6 +1,3 @@
-
-
-import { Upload } from "lucide-react";
 import { useRef } from "react";
 import { useForm } from "react-hook-form";
 import {
@@ -100,7 +97,7 @@ const CreateTrackDialog = ({ songDialogOpen, setSongDialogOpen }: CreateTrackDia
                             ) : (
                                 <>
                                     <div className="p-3 bg-zinc-800 rounded-full inline-block mb-2">
-                                        <Upload className="h-6 w-6 text-zinc-400" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-upload"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="17 8 12 3 7 8" /><line x1="12" x2="12" y1="3" y2="15" /></svg>
                                     </div>
                                     <div className="text-sm text-zinc-400 mb-2">Upload artwork</div>
                                     <button
